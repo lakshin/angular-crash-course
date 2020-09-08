757a1ba843b2c143f1c544f166a18581e248c2ec
Lakshin Karunaratne <LakshinK@99x.lk>
2020-09-08 18:32:50 +0530

    Removing unwanted component

--- AnimalApp/src/app/app-routing.module.ts
+++ AnimalApp/src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // components
-import {AnimalComponent} from './animal/animal.component';
 import {AnimalModule} from './animal/animal.module';
 
 const routes: Routes = [
   {path: '',  redirectTo: 'animal', pathMatch: 'full'},
-  {path: 'animal',  component: AnimalComponent, loadChildren: () => AnimalModule}
+  {path: 'animal', loadChildren: () => AnimalModule}
 ];
 
 @NgModule({
